994c6d259bd295603a0ab718a3114d4688e0437b
Anton Zakhar <e725f1d26ac8041f@gmail.com>
2021-02-14 18:03:24 +0300

    feat: implement transform function

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,45 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  if (!Array.isArray(arr)) throw new Error('The argument must be an array');
+
+  const result = new Array();
+  let discardItem = false;
+  let prevItemExists = false;
+
+  for (let i = 0; i < arr.length; i++) {
+    if (discardItem) {
+      discardItem = false;
+      prevItemExists = false;
+      continue;
+    }
+    
+    switch(arr[i]) {
+      case '--discard-next':
+        if ((i + 1) < arr.length) {
+          discardItem = true;
+        }
+        continue;
+      case '--discard-prev':
+        if (prevItemExists) {
+          result.pop();
+        }
+        continue;
+      case '--double-next':
+        if ((i + 1) < arr.length) {
+          result.push(arr[i + 1]);
+        }
+        continue;
+      case '--double-prev':
+        if (prevItemExists) {
+          result.push(arr[i - 1]);
+        }
+        continue;
+      default:
+        result.push(arr[i]);
+        prevItemExists = true;
+    }
+  }
+
+  return result;
 };
