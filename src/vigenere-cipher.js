073815b0319f40a5656436784c161cc50f6b6e87
Anton Zakhar <e725f1d26ac8041f@gmail.com>
2021-02-16 20:56:33 +0300

    feat: implement VigenereCipheringMachine class

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,14 +1,69 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  constructor(createDirectMachine = true) {
+    if (createDirectMachine) {
+      this.directMachine = true;
+    } else {
+      this.directMachine = false;
+    }
   }
+  encrypt(messageToEncrypt, key) {
+    if (!messageToEncrypt || !key) throw new Error();
+
+    const vigenereTable = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
+    const charsForEncrypt = messageToEncrypt.toUpperCase().split('');
+    const keysForEncrypt = key.toUpperCase().split('');
+    
+    let currentKeyIndex = 0;
+    const encryptedChars = new Array();
+    for (let i = 0; i < charsForEncrypt.length; i++) {
+      const currentChar = charsForEncrypt[i];
+      if (!vigenereTable.includes(currentChar)) {
+        encryptedChars.push(currentChar);
+        continue;
+      }
+      
+      const currentKey = keysForEncrypt[currentKeyIndex % keysForEncrypt.length];
+      currentKeyIndex += 1;
+      const currentCharIndex = vigenereTable.indexOf(currentChar);
+      const offset = vigenereTable.indexOf(currentKey);
+      const encryptedCharIndex = (currentCharIndex + offset) % vigenereTable.length;
+      const encryptedChar = vigenereTable[encryptedCharIndex];
+
+      encryptedChars.push(encryptedChar);
+    }
+
+    return this.directMachine ? encryptedChars.join('') : encryptedChars.reverse().join('');
+  }    
+  decrypt(messageToDecrypt, key) {
+    if (!messageToDecrypt || !key) throw new Error();
+
+    const vigenereTable = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
+    const charsForDecrypt = messageToDecrypt.toUpperCase().split('');
+    const keysForDecrypt = key.toUpperCase().split('');
+    
+    let currentKeyIndex = 0;
+    const decryptedChars = new Array();
+    for (let i = 0; i < charsForDecrypt.length; i++) {
+      const currentChar = charsForDecrypt[i];
+      if (!vigenereTable.includes(currentChar)) {
+        decryptedChars.push(currentChar);
+        continue;
+      }
+      
+      const currentKey = keysForDecrypt[currentKeyIndex % keysForDecrypt.length];
+      currentKeyIndex += 1;
+      const currentCharIndex = vigenereTable.indexOf(currentChar);
+      const offset = vigenereTable.indexOf(currentKey);
+      const decryptedCharIndex = (currentCharIndex + vigenereTable.length - offset) % vigenereTable.length;
+      const decryptedChar = vigenereTable[decryptedCharIndex];
+
+      decryptedChars.push(decryptedChar);
+    }
+
+    return this.directMachine ? decryptedChars.join('') : decryptedChars.reverse().join('');
+  }    
 }
 
 module.exports = VigenereCipheringMachine;
