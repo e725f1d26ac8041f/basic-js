4bf55c9d56a75e2c546d7cb73afbba4a9090fe8c
Anton Zakhar <e725f1d26ac8041f@gmail.com>
2021-03-12 23:54:36 +0300

    fix: implement correct secondsToSolve calculation in calculateHanoi function

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -3,7 +3,7 @@ const CustomError = require("../extensions/custom-error");
 module.exports = function calculateHanoi(disksNumber, turnsSpeed) {
   const numberOfMoves = (2 ** disksNumber) - 1;
   const turnsPerSecond = turnsSpeed / 3600;
-  const secondsToSolve = Math.floor(numberOfMoves * turnsPerSecond);
+  const secondsToSolve = Math.floor(numberOfMoves / turnsPerSecond);
 
   const result = new Object();
   result.turns = numberOfMoves;
